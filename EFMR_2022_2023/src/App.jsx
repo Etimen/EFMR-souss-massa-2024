25d259c555f800427353c1184f74da9a1c85e58f
Etimen <aymenelidrysy@gmail.com>
2024-11-23 20:51:55 +0100

    third commit

--- EFMR_2022_2023/src/App.jsx
+++ EFMR_2022_2023/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import InfoProduitForm from "./InfoProd";
 import Affiche from "./Affiche";
+import Recherche from "./recherche"
 
 export default function App() {
   useEffect(
@@ -17,9 +18,23 @@ export default function App() {
   console.log(produits)
   return (
      <>
-     <InfoProduitForm />
-    <Affiche produits={produits}/>
+     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/affiche">Products</Link>
+        <Link  to="/recherche">Search</Link>
+      </nav>
+    
+     <Routes>
+        <Route path="/" element={<h1>Welcome to the Product Page</h1>}/>
+        <Route path="/affiche" element={<Affiche produits={produits} />} />
+        <Route path="/recherche" element={<Recherche />} />
+        <Route path="/info-produit" element={<InfoProduitForm />} />
+     </Routes>
+    </Router>
      
+
+    
     </>
   );
 } 
